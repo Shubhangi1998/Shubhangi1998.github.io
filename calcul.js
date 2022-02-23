592306096089113391f88291fad8dbe5c3b72a2a
Shubhangi1998 <65546473+Shubhangi1998@users.noreply.github.com>
2022-02-23 12:35:51 +0530

    Added Calculator Files
    Added Calculator Files

---
+++ calcul.js
@@ -0,0 +1,27 @@
+function add(){
+	var x=document.getElementById('num1').value;
+	var y=document.getElementById('num2').value;
+	document.getElementById('out').innerHTML= parseInt(x)+ parseInt(y) ;
+	
+}
+
+function sub(){
+	var x=document.getElementById('num1').value;
+	var y=document.getElementById('num2').value;
+	document.getElementById('out').innerHTML= parseInt(x)- parseInt(y) ;
+	
+}
+function multi(){
+	var x=document.getElementById('num1').value;
+	var y=document.getElementById('num2').value;
+	document.getElementById('out').innerHTML= parseInt(x)* parseInt(y) ;
+	
+}
+function divide(){
+	var x=document.getElementById('num1').value;
+	var y=document.getElementById('num2').value;
+	document.getElementById('out').innerHTML= parseInt(x)/ parseInt(y) ;
+	
+}
+ 
+ 
